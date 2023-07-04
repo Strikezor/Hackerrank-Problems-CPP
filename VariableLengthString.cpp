acc94e3f6e04ea89b10c236144da8c10d1f83f37
Strikezor <akshaych064@gmail.com>
2023-07-04 12:27:44 +0530

    first_commit

---
+++ VariableLengthString.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int r,q;
+    cin>>r>>q;
+    vector<vector<int>> arr(r);
+    // vector<vector<int>> arr(r,vector<int> (c));
+    
+    for(int i=0;i<r;i++)
+    {
+        int n;
+        cin>>n;
+        arr[i].resize(n);
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    
+    for(int i=0;i<q;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        cout<<arr[x][y]<<endl;
+    }
+    
+    return 0;
+}
